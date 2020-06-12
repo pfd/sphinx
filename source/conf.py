01a8e48761fbdceeec8122a3132c41f8fbca1c17
Pete Davis <pete@urbanairship.com>
2020-06-12 16:07:47 -0700

    idk maybe

--- source/conf.py
+++ source/conf.py
@@ -34,6 +34,9 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -54,6 +57,11 @@ source_suffix = {
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['../_static']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
